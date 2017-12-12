2492e0b16aa8f5017872ae71f9b996f46c81c487
mala <mala.aboidje@bglgroup.co.uk>
2017-12-12 11:19:04 +0000

    Added database connection and returned some data from database

---
+++ CompetencyFrameworkAPI/CompetencyFrameworkAPI/DataAccess.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using WebGrease.Css.Ast;
+
+namespace CompetencyFrameworkAPI
+{
+    public class DataAccess
+    {
+
+        public List<string> GetAllTechnologies()
+        {
+            var technologies = new List<string> ();
+            
+
+            string connectionString = @"data source=PBO-NETSQL04\devstream1; Initial Catalog = Job_Competency_JB_MA; User Id=CompetencyFramework; Password = 1Competency2Framework3";
+            using (var connection = new SqlConnection())
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+                using (var command = new SqlCommand())
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "ReturnTechnology";
+                    command.Connection = connection;
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        technologies.Add(reader.GetString(1));
+
+                    }
+
+                }
+            }
+
+            return technologies;
+                
+        }
+    }
+}
