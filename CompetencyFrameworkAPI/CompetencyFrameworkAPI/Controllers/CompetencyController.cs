a82dfde8fb475a037d89370dc712766b3ff427e4
JohnsonBoateng <johnson.boateng@bglgroup.co.uk>
2017-12-14 11:15:16 +0000

    Implemented the get method for Technology name (using UI Engineer ) and JobTitle (using Junior UI Enigeer)

---
+++ CompetencyFrameworkAPI/CompetencyFrameworkAPI/Controllers/CompetencyController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CompetencyFrameworkAPI.Models;
+
+namespace CompetencyFrameworkAPI.Controllers
+{
+    public class CompetencyController : ApiController
+    {
+        //// GET api/values
+
+        public IEnumerable<Competency> Get(string technologyName, string jobTitle)
+        {
+            var dataAccess = new DataAccess();
+            return dataAccess.GetAllCompetencyList(technologyName, jobTitle);
+        }
+
+     
+
+    }
+}
---
+++ CompetencyFrameworkAPI/CompetencyFrameworkAPI/Models/Competency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+
+namespace CompetencyFrameworkAPI.Models
+{
+    public class Competency
+    {
+        public string TopicName { get; set; }
+        public string AreaName { get; set; }
+        public string CompetencyName { get; set; }
+        public string RatingName { get; set; }
+    }
+}
