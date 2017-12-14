3e52ee2426ec19b7ad9a95a79d2d98f7fa067acf
JohnsonBoateng <johnson.boateng@bglgroup.co.uk>
2017-12-14 13:31:05 +0000

    Implemented the get method for Technology name (using UI Engineer ) and JobTitle (using Junior UI Enigeer)

--- CompetencyFrameworkAPI/CompetencyFrameworkAPI/DataAccess.cs
+++ CompetencyFrameworkAPI/CompetencyFrameworkAPI/DataAccess.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using WebGrease.Css.Ast;
+using CompetencyFrameworkAPI.Models;
 
 namespace CompetencyFrameworkAPI
 {
@@ -144,7 +145,62 @@ namespace CompetencyFrameworkAPI
 
         }
 
+        public List<Competency> GetAllCompetencyList(string technologyName, string jobTitle)
+    {
+        var competencyList = new List<Competency>();
+
+
+        string connectionString = @"data source=PBO-NETSQL04\devstream1; Initial Catalog = Job_Competency_JB_MA; User Id=CompetencyFramework; Password = 1Competency2Framework3";
+        using (var connection = new SqlConnection())
+        {
+            connection.ConnectionString = connectionString;
+            connection.Open();
+
+            using (var command = new SqlCommand())
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "ReturnAll";
+                command.Connection = connection;
+                var parameter = new SqlParameter
+                {
+                    ParameterName = "technologyName",
+                    SqlDbType = SqlDbType.VarChar,
+                    Size = 255,
+                    Value = technologyName,
+                    Direction = ParameterDirection.Input
+                };
+
+                var technologyParameter = new SqlParameter
+                {
+                    ParameterName = "jobTitleName",
+                    SqlDbType = SqlDbType.VarChar,
+                    Size = 255,
+                    Value = jobTitle,
+                    Direction = ParameterDirection.Input
+                };
+
+                command.Parameters.Add(parameter);
+                command.Parameters.Add(technologyParameter);
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+
+                    var competency = new Competency();
+                    competency.TopicName = reader.GetString(0);
+                    competency.AreaName = reader.GetString(1);
+                    competency.CompetencyName = reader.GetString(2);
+                    competency.RatingName = reader.GetString(3);
+                    competencyList.Add((competency));
+
+                }
+
+            }
+        }
+
+        return competencyList;
+
     }
+}
 }
 
 
